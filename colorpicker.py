c6ebe4575958b5ce35b70524e25e0c19bbc2e5c5
Carlosfidelrosales <fidelcarlos302@gmail.com>
2022-11-19 13:17:20 +0800

    I added the icon of my program, its frame and also the logo.

--- colorpicker.py
+++ colorpicker.py
@@ -11,6 +11,20 @@ root.geometry("800x470+100+100")
 root.configure(bg="#e4e8eb")
 root.resizable(False,False)
 
+#icon
+imageIcon = PhotoImage(file="icon.png")
+root.iconphoto(False, imageIcon)
+
+Label(root,width=120,height=10, bg ="red").pack()
+
+#frame
+frame = Frame(root, width=700, height=370, bg="white")
+frame.place(x=50,y=50)
+
+logo=PhotoImage(file="logo.png")
+Label(frame, image=logo, bg="white").place(x=10, y=10)
+
+Label(frame, text="COLOR FINDER", font="helvetica 23 bold", bg ="white").place(x=100,y=20)
 
 
-
+root.mainloop()
