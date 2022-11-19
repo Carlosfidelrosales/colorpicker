be0d536198b13e6ca14c2d73b6bf8c62cb5179c7
Carlosfidelrosales <fidelcarlos302@gmail.com>
2022-11-19 13:07:31 +0800

    I put the import function together with pip extensions to run the program

---
+++ colorpicker.py
@@ -0,0 +1,16 @@
+from tkinter import *
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageTk
+from colorthief import ColorThief
+import os
+
+root = Tk()
+root.title("Color Picker")
+root.geometry("800x470+100+100")
+root.configure(bg="#e4e8eb")
+root.resizable(False,False)
+
+
+
+
